729642b7d0601e4eec1f4456fc3d3a394f979990
RCSKing <renansoaresfrontend@gmail.com>
2023-06-20 16:48:28 -0300

    atualizando cauculadora

---
+++ botao2/js/cauculo.js
@@ -0,0 +1,23 @@
+let a = 10;
+let b = 5;
+
+function somar(){
+    let som = a + b;
+    document.getElementById("soma").innerHTML = som;
+}
+
+function subtrair(){
+    let sub = a - b;
+    document.getElementById("subtração").innerHTML = sub;
+}
+
+function dividir(){
+    let div = a / b;
+    document.getElementById("divisão").innerHTML = div;
+}
+
+function multiplicar(){
+    let mult = a * b;
+    document.getElementById("multiplicação").innerHTML = mult;
+}
+
